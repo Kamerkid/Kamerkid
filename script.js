39318c6c7bf7212be89dffd2e91ae3c4d45c379e
Kamerkid <aliehdaisy2002@gmail.com>
2024-11-20 13:17:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
+
+
+
+
+
+
+
+
+
+
+// Typewriter effect
+
+var typed = new Typed(".typewriter-text",
+    {
+        strings: ["NURSING STUDENT", "INFLUENCER", "DANCER"],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    }
+)
